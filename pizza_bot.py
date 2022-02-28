d49608d1c12d32952553a653dd2e9e462f8563fe
DuckyToast3r <99302471+DuckyToast3r@users.noreply.github.com>
2022-03-01 09:44:59 +1300

    Created component file and welcome file
    welcome component  created  -  version1

---
+++ pizza_bot.py
@@ -0,0 +1,12 @@
+# Pizza bot program 
+import random 
+from random import randint 
+
+# List of random names 
+names = ["Mark", "Pheobe", "sally", "Michael", "Denise", "Ellen,"]
+
+num = randint(0,9)
+
+name = (names[num])
+
+print(name)
